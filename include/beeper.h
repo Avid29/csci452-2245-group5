bbeeae3d6742fc9d4c82c520a5d1ee2d7dee09aa
Mary Strodl <mstrodl@csh.rit.edu>
2025-04-22 10:41:08 -0400

    beeper stuff kinda works
    kill a bunch of userspace programs for codesize reasons...

---
+++ include/beeper.h
@@ -0,0 +1,24 @@
+/**
+** @file	beeper.h
+**
+** @author	Mary Strodl <mstrodl@csh.rit.edu>
+**
+** @brief	Beep speaker handling
+*/
+
+#ifndef BEEPER_H_
+#define BEEPER_H_
+
+#include <common.h>
+
+#ifndef ASM_SRC
+
+#ifdef KERNEL_SRC
+
+int beeper_write(char *buf, uint_t length);
+
+#endif  /* KERNEL_SRC */
+
+#endif  /* !ASM_SRC */
+
+#endif
